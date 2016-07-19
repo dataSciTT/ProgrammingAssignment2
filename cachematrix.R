37c7ca8e1b466d71e2d84416c595a2cf2d07de01
dataSciTT <tt.tai865@gmail.com>
2016-07-19 21:04:23 +0800

    Signed-off-by: dataSciTT <tt.tai865@gmail.com>

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,27 @@
+#to make matrix and able to cache inverse of matrix
 makeCacheMatrix <- function(x = matrix()) {
-  inverst <- NULL
+  inverst <- NULL 
   set <- function(y) {
     x <<- y
     inverst <<- NULL
   }
   get <- function() x
-  setInv <- function(inverstMat) inverst <<- inverstMat
-  getInv <- function() inverst
+  setInv <- function(inverstMat) inverst <<- inverstMat # set inverse into cache
+  getInv <- function() inverst # get inverse from cache
   list(set = set, get = get,
        setInv = setInv,
        getInv = getInv)
 }
 
+# to calculate the inverst of matrix if it is not yet cache or just display the inverse if it is cached
 cacheSolve <- function(mat, ...){
   inverst <- mat$getInv()
-  if(!is.null(inverst)) {
+  if(!is.null(inverst)) { # check inverse if cached
     message("getting cached data")
     return(inverst)
   }
   ori <- mat$get()
-  inverstCal <- solve(ori, ...)
+  inverstCal <- solve(ori, ...) # calculate inverse if not cached
   mat$setInv(inverstCal)
   inverstCal
 }
